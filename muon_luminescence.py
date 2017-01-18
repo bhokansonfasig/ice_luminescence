a28f6778b4519178bda4a1c9d6986286fec2ebcd
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-01-18 13:52:05 -0600

    Added command line arguments for muon_luminescence

--- muon_luminescence.py
+++ muon_luminescence.py
@@ -15,19 +15,39 @@ import argparse
 
 parser_desc = """Script for plotting hits after MinBias events from i3 files"""
 parser_ep = """Note that this script depends on the standard python libraries
-                sys, os, os.path, datetime, numpy, matplotlib.pyplot"""
+               numpy, matplotlib.pyplot; and the IceCube project's library
+               icecube"""
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description=parser_desc, epilog=parser_ep)
-
+parser.add_argument('datadir', nargs='+',
+                    help="directory containing zipped i3 files")
+parser.add_argument('-l', '--logfile',
+                    nargs='?', const='hese_lum_processing.log',
+                    help="""log file in which to write progress, instead of
+                    printing to stdout. If flag present without file
+                    name, uses 'hese_lum_processing.log'. Existing file will
+                    be overwritten""")
+parser.add_argument('-o', '--outputdir', default='.',
+                    help="""directory to place output. Defaults to current
+                    directory""")
+parser.add_argument('-k', '--keyword', default='', type=str,
+                    help="""keyword for grabbing specific files from data
+                    directory/directories (any files containing keyword)""")
+parser.add_argument('--filter', action='store_true')
 args = parser.parse_args()
 
 # Store arguments to variables for rest of the script
-
+datadirs = args.datadir
+logfilename = args.logfile
+outputdir = args.outputdir
+filekeyword = args.keyword
+filteri3 = args.filter
 
 
 # Standard libraries
-import sys, os, os.path
-import datetime
 import numpy as np
 import matplotlib.pyplot as plt
+
+# IceCube libraries
+from icecube import dataio, dataclasses
