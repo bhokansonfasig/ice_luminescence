fab88e368dcb0f3b7875a248f730096bf12efce7
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-01-18 14:06:43 -0600

    Added logfile handling to muon_luminescence

--- muon_luminescence.py
+++ muon_luminescence.py
@@ -15,15 +15,15 @@ import argparse
 
 parser_desc = """Script for plotting hits after MinBias events from i3 files"""
 parser_ep = """Note that this script depends on the standard python libraries
-               numpy, matplotlib.pyplot; and the IceCube project's library
-               icecube"""
+               sys, datatime, numpy, matplotlib.pyplot; and the IceCube
+               project's library icecube"""
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description=parser_desc, epilog=parser_ep)
 parser.add_argument('datadir', nargs='+',
                     help="directory containing zipped i3 files")
 parser.add_argument('-l', '--logfile',
-                    nargs='?', const='hese_lum_processing.log',
+                    nargs='?', const='muon_lum_processing.log',
                     help="""log file in which to write progress, instead of
                     printing to stdout. If flag present without file
                     name, uses 'hese_lum_processing.log'. Existing file will
@@ -46,8 +46,47 @@ filteri3 = args.filter
 
 
 # Standard libraries
+import sys
+import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 
 # IceCube libraries
 from icecube import dataio, dataclasses
+
+
+# Function for writing log statements
+def write_log(logline, logfilestr):
+    # Writes a line to the log file, or stdout if logfilename is None
+    # Done this way so the file updates each time a line is written
+    if logfilestr==None:
+        print(logline)
+    else:
+        with open(logfilestr, 'a') as logfile:
+            logfile.write(logline+"\n")
+            logfile.close()
+
+
+
+# Clear log file of old information
+if logfilename!=None:
+    with open(logfilename, 'w') as logfile:
+        logfile.close()
+
+# Write first lines to log file
+dirstring = ""
+for datadir in datadirs:
+    dirstring += '\n\t'+datadir
+write_log("Reading i3 files from:"+dirstring, logfilename)
+if filekeyword:
+    write_log("  filtered by keyword: "+filekeyword, logfilename)
+
+
+# Filtering files
+if filteri3:
+    write_log("Filtering files and placing in: "+outputdir, logfilename)
+
+
+# Processing files
+else:
+    write_log("Outputting to: "+outputdir, logfilename)
