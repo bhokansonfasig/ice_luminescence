3d37800abf063d06a4c2da7fdf4ee9cfadc4fb4f
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-01-26 15:44:37 -0600

    Changed to analyze all P frames per Q frame

--- muon_luminescence.py
+++ muon_luminescence.py
@@ -163,7 +163,7 @@ else:
         infiles.extend(grab_filenames(directory,filekeyword))
 
     bin_width = 1000
-    time_limit = 1000000
+    time_limit = 10000000
     n_bins = time_limit/bin_width
 
     hits_histogram = np.zeros(n_bins)
@@ -202,8 +202,8 @@ else:
         # in the trigger window for dividing out later
         for event in minbias_events:
             q_frame = event[0]
+            frame_not_analyzed = True
             for p_frame in event[1:]:
-                frame_not_analyzed = True
                 if 'I3TriggerHierarchy' in p_frame:
                     for key,value in p_frame['I3TriggerHierarchy'].iteritems():
                         if value.key.type==value.key.type.MERGED:
@@ -233,7 +233,7 @@ else:
 
                     total_events += 1
                     frame_not_analyzed = False
-                    break
+                    # break
 
             if frame_not_analyzed:
                 write_log("  I3TriggerHierarchy not found in frame",logfilename)
