2120f7f42869e14ed5e3a14b81fe55fe6adf351b
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-05-16 16:23:47 -0500

    Split icetray file into two files, one to get minbias frames and the other to calculate charge

---
+++ get_minbias_frames.py
@@ -0,0 +1,110 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py2-v2/icetray-start
+#METAPROJECT offline-software/trunk
+#
+# get_minbias_frames.py
+# Script for pulling frames from i3 files that pass the minbias filter
+#
+#
+# Ben Hokanson-Fasig
+# Created   05/16/17
+# Last edit 05/16/17
+
+
+from __future__ import division, print_function
+import argparse
+
+parser_desc = """Script for pulling frames from i3 files that pass the minbias filter"""
+parser_ep = """Note that this script depends on the standard python library
+               os; and the IceCube project's custom libraries icecube, I3Tray"""
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description=parser_desc, epilog=parser_ep)
+parser.add_argument('datadir', nargs='+',
+                    help="directories containing i3 files")
+parser.add_argument('-g', '--gcdfile', default='',
+                    help="""GCD file corresponding to i3 files in datadir.
+                    If not provided and only one file containing 'GCD' is
+                    found in the directory, it will be used""")
+parser.add_argument('-o', '--outfile', default='./minbias_frames.i3',
+                    help="""output file name. Defaults to 'minbias_frames.i3' in
+                    current directory""")
+parser.add_argument('-k', '--keyword', nargs='+', default=[''], type=str,
+                    help="""keyword(s) for grabbing specific files from data
+                    directory/directories (any files containing the keyword)""")
+parser.add_argument('--antikeyword', nargs='+', type=str,
+                    default=['thisISanANTIKEYWORDandHOPEFULLYitISlongANDobscureENOUGHthatNOfileCOULDpossiblyHAVEit'],
+                    help="""keyword(s) for grabbing specific files from data
+                    directory/directories (any files NOT containing the
+                    antikeyword)""")
+args = parser.parse_args()
+
+# Store arguments to variables for rest of the script
+datadirs = args.datadir
+gcdfilename = args.gcdfile
+outfilename = args.outfile
+filekeywords = args.keyword
+fileantikeywords = args.antikeyword
+
+
+# Standard libraries
+import os
+
+# IceCube libraries
+from icecube import icetray
+from I3Tray import I3Tray
+
+
+def grab_filenames(datadir,keywords,antikeywords):
+    """Returns a list of file names in datadir containing the keyword"""
+    allfiles = os.listdir(datadir)
+    matching = []
+    for filename in allfiles:
+        match = bool(".i3" in filename)
+        for keyword in keywords:
+            match = match and (keyword in filename)
+        for antikeyword in antikeywords:
+            match = match and not(antikeyword in filename)
+        if match:
+            matching.append(os.path.join(datadir,filename))
+    return sorted(matching)
+
+
+
+# Filtering function
+def minBiasOnly(frame):
+    """Passes only frames which pass the min bias filter"""
+    if 'QFilterMask' not in frame:
+        return False
+    for filtername,result in frame['QFilterMask'].iteritems():
+        if ('FilterMinBias' in filtername) and not('SDST' in filtername):
+            if result.condition_passed and result.prescale_passed:
+                return True
+    return False
+
+
+# Try to find GCD file if none provided
+if not(gcdfilename) and len(datadirs)==1:
+    possiblegcd = grab_filenames(datadirs[0],"GCD",fileantikeywords)
+    if len(possiblegcd)==1:
+        gcdfilename = possiblegcd[0]
+        print("Using found GCD file",gcdfilename)
+
+# Make GCD file the first input file
+if gcdfilename:
+    infiles = [gcdfilename]
+else:
+    infiles = []
+
+# Grab the rest of the input files
+for directory in datadirs:
+    infiles.extend(grab_filenames(directory,filekeywords,fileantikeywords))
+
+
+# Use icetray to filter all the minbias frames into a single file
+tray = I3Tray()
+tray.Add('I3Reader',"reader",FileNameList=infiles)
+tray.Add(minBiasOnly,"filter",Streams=[icetray.I3Frame.DAQ,icetray.I3Frame.Physics])
+tray.Add('I3Writer',"writer",FileName=outfilename)
+tray.Execute()
+tray.Finish()
+
---
+++ muon_lum_charges.py
@@ -0,0 +1,112 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py2-v2/icetray-start
+#METAPROJECT offline-software/trunk
+#
+# muon_lum_charges.py
+# Script for calculating event and late charges from muon events
+#
+#
+# Ben Hokanson-Fasig
+# Created   05/16/17
+# Last edit 05/16/17
+
+
+from __future__ import division, print_function
+import argparse
+
+parser_desc = """Script for calculating event and late charges from muon events"""
+parser_ep = """Note that this script depends on the IceCube project's custom
+               libraries icecube, I3Tray"""
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description=parser_desc, epilog=parser_ep)
+parser.add_argument('infile', nargs='+',
+                    help="input i3 file of muon events")
+parser.add_argument('-o', '--outfile', default='./muon_lum_charges.i3',
+                    help="""output file name. Defaults to 'muon_lum_charges.i3'
+                    in current directory""")
+args = parser.parse_args()
+
+# Store arguments to variables for rest of the script
+infilename = args.infile
+outfilename = args.outfile
+
+
+# Standard libraries
+
+
+# IceCube libraries
+from icecube import icetray, dataclasses
+from icecube.phys_services import I3Calculator
+from I3Tray import I3Tray
+
+
+
+# Processing module
+class ChargeModule(icetray.I3Module):
+    """Module for calcualting event and late charges of physics event"""
+    def __init__(self,context):
+        icetray.I3Module.__init__(self,context)
+        self.AddOutBox("OutBox")
+
+    def Configure(self):
+        """Set a default DOM geometry"""
+        self.omgeo = {}
+
+    def calculateCharges(self,frame):
+        """Calculate the event and late charges"""
+        if 'I3TriggerHierarchy' not in frame:
+            return False
+        if 'InIcePulses' not in frame:
+            return False
+        if 'SPEFitSingle' not in frame:
+            return False
+
+        for key,value in frame['I3TriggerHierarchy'].iteritems():
+            if value.key.type==value.key.type.MERGED:
+                trigger_window = value
+                break
+
+        # Ignore events with trigger window larger than 15 microseconds
+        # Should cut out coincident muons and slow particle triggers
+        if trigger_window.length>15000:
+            return False
+
+        pulse_map = dataclasses.I3RecoPulseSeriesMap.from_frame(frame,'InIcePulses')
+
+        fit_particle = frame['SPEFitSingle']
+
+        event_charge = 0
+        late_charge = 0
+        for om,pulses in pulse_map.iteritems():
+            for pulse in pulses:
+                t_res = I3Calculator.time_residual(fit_particle,self.omgeo[om].position,pulse.time)
+                if t_res>-75 and t_res<1000:
+                    event_charge += pulse.charge
+                elif t_res>2000:
+                    late_charge += pulse.charge
+
+        frame["event_charge"] = dataclasses.I3Double(event_charge)
+        frame["late_charge"] = dataclasses.I3Double(late_charge)
+
+        return True
+
+
+    def Geometry(self,frame):
+        """Set up the DOM geometry when reaching G-frame"""
+        self.omgeo = frame['I3Geometry'].omgeo
+        self.PushFrame(frame)
+
+    def Physics(self,frame):
+        """Calculate charges (if possible) for each P-frame"""
+        if self.calculateCharges(frame):
+            self.PushFrame(frame)
+
+
+
+tray = I3Tray()
+tray.Add('I3Reader',"reader",FileNameList=infilename)
+tray.Add(ChargeModule)
+tray.Add('I3Writer',"writer",FileName=outfilename,
+         DropOrphanStreams=[icetray.I3Frame.Calibration,icetray.I3Frame.DAQ])
+tray.Execute()
+tray.Finish()
